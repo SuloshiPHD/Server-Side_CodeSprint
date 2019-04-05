40d4de0185b02aa34a639d89438271565ac2ce5a
prashan12345 <39889610+prashan12345@users.noreply.github.com>
2019-04-05 19:52:14 +0530

    Add files via upload

--- CodeSprint/logout.php
+++ CodeSprint/logout.php
@@ -5,14 +5,14 @@ echo "<link rel=stylesheet type=text/css href=mystylesheet.css>"; //Call in styl
 echo "<title>".$pagename."</title>"; //display name of the page as window title
 echo "<body>";
 include ("headfile.html"); //include header layout file
-include ("detectlogin.php"); 
+
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
-echo "Thank you for shopping at Hometeq<br>";
-// remove all session variables 
+//display random text
+echo "<p> Thank you ".$_SESSION['fname']."   ".$_SESSION['lname'];
 session_unset();
-// destroy the session 
-session_destroy(); 
-echo "You are now logged out";
+session_destroy();
+echo "<P> You have successfully logged out";
+
 include("footfile.html"); //include head layout
 echo "</body>";
 ?>
